d7403c73ca66cdf7e98a246564b1c7f356aaa4e7
Sudip  Biswas <sudipbiswas322@gmail.com>
2020-08-30 03:17:46 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My first App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
