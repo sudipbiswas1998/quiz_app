7980275931b0ecdfeeb2ac1f56abfee9e4a7bf21
Sudip  Biswas <sudipbiswas322@gmail.com>
2020-08-31 21:08:23 +0530

    Final

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 I  am  on  my learning stage. And this is my first app  so dont judge  me...  :)
 
+**What I learned from the project
+*Dart basics
+*Flutter basics
+*Building a widget tree
+*Column
+*RaisedButton
+*StatefullWidget, StateLessWidget
+*Building custom StateLessWidget
+*Splitting into Widget
+*Map in Dart
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer chosen');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your faviorite colour?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your faviorite animal?',
+      'answer': [
+        {'text': 'Tiger', 'score': 10},
+        {'text': 'Lion', 'score': 5},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Dog', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite fruit',
+      'answer': [
+        {'text': 'Orange', 'score': 10},
+        {'text': 'Apple', 'score': 5},
+        {'text': 'Mango', 'score': 3},
+        {'text': 'Banana', 'score': 1},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex += 1;
+    });
+    print(_questionIndex);
   }
 
   @override
@@ -16,25 +70,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My first App'),
         ),
-        body: Column(
-          children: [
-            Text('The question'),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                print('Answer 3 chosen!');
-              },
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
